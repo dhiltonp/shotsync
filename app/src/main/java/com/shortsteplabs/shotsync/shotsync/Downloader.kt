a467eec6b7c74e35679c6f3834555c2ec7f93450
David P Hilton <david.hilton.p@gmail.com>
2018-03-31 11:39:19 -0400

    downloads one image and shuts down

--- app/src/main/java/com/shortsteplabs/shotsync/shotsync/Downloader.kt
+++ app/src/main/java/com/shortsteplabs/shotsync/shotsync/Downloader.kt
@@ -3,6 +3,11 @@ package com.shortsteplabs.shotsync.shotsync
 import android.app.IntentService
 import android.content.Intent
 import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.*
 
 /**
  * An [IntentService] subclass for handling asynchronous task requests in
@@ -13,12 +18,14 @@ import android.content.Context
  * helper methods.
  */
 class Downloader : IntentService("Downloader") {
+    private val TAG = "Downloader"
     // intent:
     //  receive that we have connected/disconnected
     //  download files
-    //  put notfication of DL status in foreground
+    //  put notification of DL status in foreground
 
     var downloading = false
+    var queue: RequestQueue? = null
 
     override fun onHandleIntent(intent: Intent?) {
         if (intent != null) {
@@ -31,42 +38,55 @@ class Downloader : IntentService("Downloader") {
         }
     }
 
+    private fun startQueue() {
+        if (queue == null) {
+            val cache = DiskBasedCache(cacheDir, 1024 * 1024)
+            val network = BasicNetwork(HurlStack())
+            queue = RequestQueue(cache, network, 1).apply {
+                start()
+            }
+        }
+    }
+
     private fun handleStartDownload() {
         if (!downloading) {
+            Log.d(TAG, "Starting download")
             downloading = true
-//                // If using Volley, will need to tell it to just use one downloader.
-//                val queue = Volley.newRequestQueue(context);
-//
-//                // list images
-//
-//                // dl images
-//                val imageRequest = StringRequest(Request.Method.GET, "http://192.168.0.10/DCIM/117OLYMP/P3272208.ORF",
-//                        Response.Listener<String> { response ->
-//                            Log.d(TAG, "img downloaded, " + response.length + " bytes")
-//                        },
-//                        Response.ErrorListener {
-//                            Log.d(TAG, "img failed to download!")
-//                        })
-//                queue.add(imageRequest)
-//
-//
+
+            startQueue()
+
+            // list images
+
+            // dl images
+            val imageRequest = StringRequest(Request.Method.GET, "http://192.168.0.10/DCIM/117OLYMP/P3272208.ORF",
+                    Response.Listener<String> { response ->
+                        Log.d(TAG, "img downloaded, " + response.length + " bytes")
+                    },
+                    Response.ErrorListener {
+                        Log.d(TAG, "img failed to download!")
+                    })
+            imageRequest.tag = TAG
+            queue?.add(imageRequest)
+
+            // may have to also disconnect from the network to prevent having to enter password every time...
+            val stringRequest = StringRequest(Request.Method.GET, "http://192.168.0.10/exec_pwoff.cgi",
+                    Response.Listener<String> { response ->
+                        Log.d(TAG, "Camera off")
+                    },
+                    Response.ErrorListener {
+                        Log.d(TAG, "Failed to turn off camera!")
+                    })
+            imageRequest.tag = TAG
+            queue?.add(stringRequest)
         }
     }
 
     private fun handleStopDownload() {
         if (downloading) {
+            Log.d(TAG, "Stopping download")
             downloading = false
-//                // shutdown camera:
-//                // may have to also disconnect from the network to prevent having to enter password every time...
-//                val stringRequest = StringRequest(Request.Method.GET, "http://192.168.0.10/exec_pwoff.cgi",
-//                        Response.Listener<String> { response ->
-//                            Log.d(TAG, "Camera off")
-//                        },
-//                        Response.ErrorListener {
-//                            Log.d(TAG, "Failed to turn off camera!")
-//                        })
-//                // can't just queue it up, requests run in parallel
-//                //queue.add(stringRequest)
+            queue?.cancelAll(TAG)
+            queue?.stop()
         }
     }
 
